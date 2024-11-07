4fb9a207e303c3192ba1877a7f4e63006a7336f2
Grace Kwak <gracekwak25@gmail.com>
2024-11-07 18:27:09 -0500

    Finish implementing count_alive_neighbors()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-// use rand::prelude::*;
-
 struct Grid {
   cells: Vec<Vec<Liveness>>,
+  size: (usize, usize)
 }
 
 impl Grid {
@@ -23,23 +22,33 @@ impl Grid {
 
   fn count_alive_neighbors(&self, x: usize, y: usize) -> usize {
     let mut count = 0;
-    if self.cells[x+1][y] == Liveness::Alive {
-      count += 1;
+
+    if x >= self.size.0 || y >= self.size.1 {
+        panic!("Cell of interest is out of bounds");
     }
 
-    if self.cells[x-1][y] == Liveness::Alive {
-      count += 1;
+    let deltas = [
+        (-1, -1), (0, -1), (-1, 1),
+        (-1, 0),           (0, 1),
+        (-1, 1),  (0, 1),  (1, 1)
+        ];
+
+    for (dx, dy) in &deltas {
+        // Calculate the cell coords to look for
+        let new_x = x as isize + dx;
+        let new_y = y as isize + dy;
+
+        // Check if the cell coords are within bounds
+        let x_valid = new_x >= 0 && new_x < self.size.0 as isize;
+        let y_valid = new_y >= 0 && new_y < self.size.1 as isize;
+        if x_valid && y_valid {
+            // println!("Checking cell at ({}, {})", new_x, new_y);
+            // Check if the cell is alive
+            if self.cells[new_x as usize][new_y as usize] == Liveness::Alive {
+                count += 1;
+            }
+        }
     }
-
-    // x+1 , y
-    // x-1 , y
-    // x , y+1
-    // x , y-1
-    // x-1, y-1
-    // x+1, y+1
-    // x-1, y+1
-    // y-1, x+1
-
     count
   }
 }
@@ -59,7 +68,8 @@ fn random_grid(width: usize, height: usize) -> Grid {
   }
 
   Grid {
-    cells
+    cells: cells,
+    size: (width, height)
   }
 }
 
@@ -72,4 +82,5 @@ enum Liveness {
 fn main() {
   let g = random_grid(6,6);
   g.print();
+  println!("Num alive neighbors: {}", g.count_alive_neighbors(1,1));
 }
